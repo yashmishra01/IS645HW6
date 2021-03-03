5d7a2acfde05337296eb5db65ab56270df6a323b
yashmishra01 <yam2810@gmail.com>
2021-03-02 22:41:55 -0600

    2nd

--- js/ex2.js
+++ js/ex2.js
@@ -1,7 +1,14 @@
-console.log("question 2 solution");
-const displayText400 = document.getElementsByClassName("400level");
-console.log(displayText400);
+// console.log("question 2 solution");
+// const displayText400 = document.getElementsByClassName("400level");
+// console.log(displayText400);
 
-for (i = 0; i <= displayText400.length; i++) {
-    console.log(displayText400[i].textContent);
+// for (i = 0; i <= displayText400.length; i++) {
+//     console.log(displayText400[i].textContent);
+// }
+
+const displayText400 = () => {
+    const level400Items = Array.from(document.getElementsByClassName("400level"));
+    console.log(level400Items)
+    level400Items.forEach(item => console.log(item.innerHTML));
 }
+displayText400();
--- js/ex6.js
+++ js/ex6.js
@@ -1,6 +1,6 @@
 console.log("question 6 solution");
 
-const div = document.getElementById("content");
+
 const words = [{
     term: "Procrastination",
     definition: "Avoidance of doing a task that needs to be accomplished"
@@ -11,3 +11,21 @@ const words = [{
     term: "Oxymoron",
     definition: "figure of speech that juxtaposes elements that appear to be contradictory"
 }];
+
+const dlItem = document.createElement("dl");
+words.forEach(dl => {
+    const dtItem = document.createElement("dt");
+    const ddItem = document.createElement("dd");
+
+    dtItem.textContent = dl.term;
+    ddItem.textContent = dl.definition;
+
+    dtItem.style.fontWeight = "bold";
+
+    dlItem.appendChild(dtItem);
+    dlItem.appendChild(ddItem);
+
+});
+
+const divItem = document.getElementById("content");
+divItem.appendChild(dlItem);
